046eb34e1349f9a88b8b8b7c05b378ff15bbe6be
Francisco Rangel <francorangelcoronado@gmail.com>
2022-02-28 16:20:51 -0600

    Agregando docker y docker compose, funcional al 100

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,9 @@ config()
 
 const configuration = {
     PORT: process.env.PORT || 3000,
-    MONGODB_HOST: process.env.MONGODB_HOST || 'localhost',
+    MONGODB_HOST: process.env.MONGODB_HOST || 'mongo',
     MONGODB_DATABASE: process.env.MONGODB_DATABASE || 'TodoDB',
-    MONGODB_URI: `mongodb://${process.env.MONGODB_HOST || "localhost"}/${
+    MONGODB_URI: `mongodb://${process.env.MONGODB_HOST || "mongo"}/${
         process.env.MONGODB_DATABASE || "TodoDB"
     }`,
 }
